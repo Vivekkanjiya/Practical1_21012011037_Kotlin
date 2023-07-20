a43231b6d5e4821295a5671e70c318bdd1059567
Vivek <vivekkanjiya55@gmail.com>
2023-07-20 07:48:12 +0530

    Practical1_21012011037_Kotlin

---
+++ src/main/kotlin/PracticaL1_7.kt
@@ -0,0 +1,15 @@
+fun main(){
+    println("Enter the num")
+    var num = readln()!!.toInt()
+    var result = fact(num)
+    println("fact = "+result)
+}
+
+fun fact(num:Int):Int {
+    if (num == 0 || num == 1) {
+        return 1
+    }
+    else{
+        return num * fact(num-1)
+    }
+}
